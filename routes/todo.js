3d67e6ce3aebe35aac544a1aa8191c7d53ed0b6e
Nicola Gasparro <nicola.gasp11@gmail.com>
2020-03-16 10:09:43 +0000

    modified routes todo.js and views new.hbs

--- routes/todo.js
+++ routes/todo.js
@@ -16,6 +16,38 @@ router.get('/new', (req, res) => {
   res.render('new');
 });
 
+function validTodo(todo) {
+  return typeof todo.title == 'string' && 
+         todo.title.trim() != '' && 
+         typeof todo.priority != 'undefined' &&
+         !isNaN(Number(todo.priority));
+}
+
+router.post('/', (req, res) => {
+  console.log(req.body);
+  if(validTodo(req.body)) {
+  const todo = {
+    title: req.body.title,
+    description: req.body.description,
+    priority: req.body.priority,
+    date: new Date()
+  };
+// insert into the database 
+  knex('todo')
+    .insert(todo, 'id')
+    .then(ids => {
+      const id = ids[0];
+      res.redirect(`/todo/${id}`);
+    });
+} else {
+  //response with a error
+  res.status(500);
+  res.render('error', {
+    message: 'Invalid todo'
+  });
+  }
+});
+
 
 module.exports = router;
  
