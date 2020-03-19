7ad5287891eb3a07e70a60776fb5a4665bdd5322
Nicola Gasparro <nicola.gasp11@gmail.com>
2020-03-19 10:06:27 +0000

    modified app.js, todo.js, all.hbs, add views edit.hbs, single.hbs

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+const hbs = require('hbs');
+
+hbs.registerHelper('select', function(selected, options){
+  return options.fn(this).replace(
+    new RegExp(' value=\"' + selected + '\"'),
+    '$& selected="selected"');
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/todo.js
+++ routes/todo.js
@@ -12,16 +12,48 @@ router.get('/', (req, res) => {
   }); 
 });
 
+
+function respondAndRenderTodo(id, res, viewName){
+    if(typeof id != 'undefined'){
+      knex('todo')
+          .select()
+          .where('id', id)
+          .first()
+          .then(todo => {
+            res.render(viewName, todo);
+          });
+    } else {
+        res.status(500);
+        res.render('error', {
+          message: 'Invalid id'
+        });
+      }
+};
+
+
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  respondAndRenderTodo(id, res, 'single');
+});
+
+router.get('/:id/edit', (req, res) => {
+  // get todo with the id in the url  
+  const id = req.params.id;
+  respondAndRenderTodo(id, res, 'edit');
+});
+
+
 router.get('/new', (req, res) => {
   res.render('new');
 });
+
 
 function validTodo(todo) {
   return typeof todo.title == 'string' && 
          todo.title.trim() != '' && 
          typeof todo.priority != 'undefined' &&
          !isNaN(Number(todo.priority));
-}
+};
 
 router.post('/', (req, res) => {
   console.log(req.body);
@@ -31,7 +63,7 @@ router.post('/', (req, res) => {
     description: req.body.description,
     priority: req.body.priority,
     date: new Date()
-  };
+};
 // insert into the database 
   knex('todo')
     .insert(todo, 'id')
@@ -46,6 +78,10 @@ router.post('/', (req, res) => {
     message: 'Invalid todo'
   });
   }
+});
+
+router.put('/:id', (req, res) => {
+
 });
 
 
